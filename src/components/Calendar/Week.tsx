35cc31b8edb2c48c7ec5c019cbeec47cc554d9ef
Heecheon Park <heecheon@thesesamelab.com>
2023-09-07 17:12:27 +0900

    removed top border on week component

--- src/components/Calendar/Week.tsx
+++ src/components/Calendar/Week.tsx
@@ -33,7 +33,7 @@ const Week: React.FC = () => {
     }, [startWeekOn]);
 
     return (
-        <div className="grid grid-cols-7 py-2 border-gray-300 border-y dark:border-gray-700">
+        <div className="grid grid-cols-7 py-2 border-b border-gray-300 dark:border-gray-700">
             {DAYS.map(item => (
                 <div key={item} className="tracking-wide text-center text-gray-500">
                     {ucFirst(
